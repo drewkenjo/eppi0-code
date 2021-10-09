07d9533e70284f35f1a15f01f1140640681ebde5
Andrey <kenjo@jlab.org>
2021-10-09 11:24:21 -0400

    refactoring

--- lvl2_ntuple_eppi0.groovy
+++ lvl2_ntuple_eppi0.groovy
@@ -42,211 +42,167 @@ if(ismc) suff += '.mc'
 else suff += '.qa'
 
 def ff = new ROOTFile("lvl2_eppi0.${suff}.root")
-def tt = ff.makeNtuple('h22','title','ihel:ex:ey:ez:px:py:pz:g1x:g1y:g1z:g2x:g2y:g2z:idet:esec:psec:g1sec:g2sec:run:status:px0:py0:pz0:dcx1:dcy1:dcz1:g1v:g1w:g2v:g2w')
+def varlist = 'ex:ey:ez:px:py:pz:g1x:g1y:g1z:g2x:g2y:g2z:esec:psec:g1sec:g2sec:run:status'
+
+if(ismc) varlist += ':ex0:ey0:ez0:px0:py0:pz0'
+else varlist += 'ihel:dcx1:dcy1:dcz1:g1v:g1w:g2v:g2w'
+
+def tt = ff.makeNtuple('h22','title',varlist)
 
 GParsPool.withPool 12, {
 args.eachParallel{fname->
-  println(fname)
-  QADB qa = new QADB()
+	println(fname)
+	QADB qa = new QADB()
 
-  def reader = new HipoReader()
-  reader.open(fname)
-  def event = new Event()
-  def factory = reader.getSchemaFactory()
-  def banks = ['RUN::config','REC::Event','REC::Particle','REC::Calorimeter','REC::Cherenkov','REC::Traj','REC::Scintillator'].collect{new Bank(factory.getSchema(it))}
+	def reader = new HipoReader()
+	reader.open(fname)
+	def event = new Event()
+	def factory = reader.getSchemaFactory()
+	def banks = ['RUN::config','REC::Event','REC::Particle','REC::Calorimeter','REC::Cherenkov','REC::Traj','REC::Scintillator'].collect{new Bank(factory.getSchema(it))}
 
-  while(reader.hasNext()) {
-    reader.nextEvent(event)
-    banks.each{event.read(it)}
+	while(reader.hasNext()) {
+		reader.nextEvent(event)
+		banks.each{event.read(it)}
 
-    if(banks.every()) {
-//    if(banknames.every{event.hasBank(it)}) {
-/*
-      def mcpro = 0, mcele = 0
-      if(event.hasBank("MC::Particle")) {
-        def mcb = event.getBank('MC::Particle')
-mcpro = LorentzVector.withPID(2212,*['px','py','pz'].collect{mcb.getFloat(it,1)})
-mcele = LorentzVector.withPID(11,*['px','py','pz'].collect{mcb.getFloat(it,0)})
-      }
-*/
+		if(banks.every()) {
+			def (runb,evb,partb,ecb,ccb,trajb,scb) = banks
 
-      def (runb,evb,partb,ecb,ccb,trajb,scb) = banks
+			def run = runb.getInt("run",0)
+			def evn = runb.getInt("event",0)
 
-      def run = runb.getInt("run",0)
-      def evn = runb.getInt("event",0)
+			if(ismc || qa.OkForAsymmetry(run, evn))
+			if(true) {
 
-      if(ismc || qa.OkForAsymmetry(run, evn))
-      if(true) {
-        def combs = (0..<partb.getRows()).findAll{partb.getShort('status',it)<0 && partb.getInt('pid',it)==11}
-          .collect{ElectronCandidate.getElectronCandidate(it, partb, ecb, ccb, trajb, isinb)}.findAll{it.iselectron(*elecuts)}
-          .collectMany{canele->
-            //(0..<partb.getRows()).findAll{partb.getInt('pid',it)==2212}.collect{ipro->[iele,esec,ipro]}
-            (0..<partb.getRows()).collect{ProtonCandidate.getProtonCandidate(it, partb, trajb, isinb)}.findAll{it.isproton(*procuts)}.collect{canpro->[canele,canpro]}
-          }.collectMany{canele,canpro->
-            //(0..<partb.getRows()-1).findAll{partb.getInt('pid',it)==22}.collectMany{ig1->
-            //  (ig1+1..<partb.getRows()).findAll{partb.getInt('pid',it)==22}.collect{ig2->[iele,esec,ipro,psec,ig1,ig2]}
-            //(0..<partb.getRows()-1).findAll{PhotonCandidate.getPhotonCandidate(it, partb, ecb, isinb).isphoton()}.collectMany{ig1->
-            //  (ig1+1..<partb.getRows()).findAll{PhotonCandidate.getPhotonCandidate(it, partb, ecb, isinb).isphoton()}.collect{ig2->[iele,esec,ipro,psec,ig1,ig2]}
-            (0..<partb.getRows()-1).collect{PhotonCandidate.getPhotonCandidate(it, partb, ecb, isinb)}.findAll{it.isphoton(*phocuts) && it.getPCALsector()}.collectMany{cang1->
-              (cang1.ipart+1..<partb.getRows()).collect{PhotonCandidate.getPhotonCandidate(it, partb, ecb, isinb)}.findAll{it.isphoton(*phocuts) && it.getPCALsector()}.collect{cang2->[canele,canpro,cang1,cang2]}
-            }
-          }.findResults{canele,canpro,cang1,cang2->
-            int ipro = canpro.ipart, ig1 = cang1.ipart, ig2 = cang2.ipart
-            int g1sec = cang1.getPCALsector(), g2sec = cang2.getPCALsector()
-            int iele = canele.ipart, esec = canele.getPCALsector()
+				def elecan = ElectronCandidate.getElectronCandidate(0, partb, ecb, ccb, trajb, isinb)
 
-            def ele = LorentzVector.withPID(11,*['px','py','pz'].collect{partb.getFloat(it,iele)})
-            def pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{partb.getFloat(it,ipro)})
-            def fepx = (beam+target-ele-pro).mass2()
+				def procans = []
+				if(elecan.iselectron(*elecuts))
+					procans = (0..<partb.getRows()).collect{ProtonCandidate.getProtonCandidate(it, partb, trajb, isinb)}.findAll{it.isproton(*procuts)}
 
-            def g1 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig1)})
-            def g2 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig2)})
+				def gcans = []
+				if(procans)
+					gcans = (1..<partb.getRows()).collect{PhotonCandidate.getPhotonCandidate(it, partb, ecb, isinb)}.findAll{it.isphoton(*phocuts) && it.getPCALsector()}
 
-            def dgsec = 10
-            if(g1sec && g2sec) {
-              dgsec = (g2sec-g1sec).abs()
-              if(dgsec>3) dgsec = 6-dgsec
-            }
+				if(gcans.size()>1) {
+					int iele = elecan.ipart, esec = elecan.getPCALsector()
+					def ele = elecan.getLorentzVector()
 
-            def fgg = (g1+g2).mass()
+					def pi0cans = [gcans, gcans].combinations().findAll{it[1].ipart > it[0].ipart}.findAll{g1can,g2can->
+						int ig1 = g1can.ipart, ig2 = g2can.ipart
+						int g1sec = g1can.getPCALsector(), g2sec = g2can.getPCALsector()
 
-            return dgsec==0 && esec!=g1sec && esec!=g2sec && g1.e()>0.6 && g2.e()>0.6 &&
-            fgg>0.07 && fgg<0.2 ? [canele, canpro, cang1, cang2] :  null
-          }
+						def g1 = g1can.getLorentzVector()
+						def g2 = g2can.getLorentzVector()
 
-        def finals = combs.findResults{
-          def (canele,canpro,cang1,cang2) = it
-          int iele = canele.ipart, esec = canele.getPCALsector()
-          int ipro = canpro.ipart, psec = canpro.getDCsector()
-          int ig1 = cang1.ipart, g1sec = cang1.getPCALsector()
-          int ig2 = cang2.ipart, g2sec = cang2.getPCALsector()
+						def dgsec = 10
+						if(g1sec && g2sec) {
+							dgsec = (g2sec-g1sec).abs()
+							if(dgsec>3) dgsec = 6-dgsec
+						}
 
-          def ele = LorentzVector.withPID(11,*['px','py','pz'].collect{partb.getFloat(it,iele)})
-          def pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{partb.getFloat(it,ipro)})
-          def g1 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig1)})
-          def g2 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig2)})
+						def fgg = (g1+g2).mass()
 
-          def g1ecn = (0..<ecb.getRows()).findAll{ecb.getShort("pindex",it)==ig1 && ecb.getByte("detector",it)==DetectorType.ECAL.getDetectorId()}.size()
-          def g2ecn = (0..<ecb.getRows()).findAll{ecb.getShort("pindex",it)==ig2 && ecb.getByte("detector",it)==DetectorType.ECAL.getDetectorId()}.size()
+						return dgsec==0 && esec!=g1sec && esec!=g2sec && g1.e()>0.6 && g2.e()>0.6 && fgg>0.07 && fgg<0.2
+					}
 
-          def dcx1=canpro.getDC1x(), dcy1=canpro.getDC1y(), dcz1=canpro.getDC1z()
+					def finals = []
+					if(pi0cans) finals = [procans, pi0cans].combinations().findResults{procan, ggcans->
+						def (g1can, g2can) = ggcans
 
+						int ipro = procan.ipart, ig1 = g1can.ipart, ig2 = g2can.ipart
+						int psec = procan.getDCsector(), g1sec = g1can.getPCALsector(), g2sec = g2can.getPCALsector()
 
-  //if(mcpro!=0) pro = mcpro
-  //if(mcele!=0) ele = mcele
+						def pro = procan.getLorentzVector()
+						def g1 = g1can.getLorentzVector()
+						def g2 = g2can.getLorentzVector()
 
-          def epx = beam+target-ele-pro
-          def gg = g1+g2
-          def epggx = epx-gg
+						def epx = beam+target-ele-pro
+						def gg = g1+g2
+						def epggx = epx-gg
 
-          def feggx = (beam+target-ele-gg).mass()
-          def fgg = gg.mass()
-          def fepx = epx.mass2()
-          def fmisse = epggx.e()
-          def ftheta = epx.vect().theta(gg.vect())
-          def dpx = epggx.px().abs()
-          def dpy = epggx.py().abs()
-          def dpt = Math.sqrt(dpx*dpx + dpy*dpy)
-          def dpz = epggx.pz()
+						def feggx = (beam+target-ele-gg).mass()
+						def fgg = gg.mass()
+						def fepx = epx.mass2()
+						def fmisse = epggx.e()
+						def ftheta = epx.vect().theta(gg.vect())
+						def dpx = epggx.px().abs()
+						def dpy = epggx.py().abs()
+						def dpt = Math.sqrt(dpx*dpx + dpy*dpy)
+						def dpz = epggx.pz()
 
-          def dphi = Math.toDegrees(gg.phi()-epx.phi()).abs()
-//          def fpz = epggx.pz()
+						def dphi = Math.toDegrees(gg.phi()-epx.phi()).abs()
+	//          def fpz = epggx.pz()
 
-          def ww = (beam+target-ele).mass()
-          def q2 = -(beam-ele).mass2()
+						def ww = (beam+target-ele).mass()
+						def q2 = -(beam-ele).mass2()
 
-          def px0=0, py0=0,pz0=0
-          def idet = (partb.getShort('status',ipro)/2000).toInteger()
-          def ihel = evb.getByte("helicity",0)
-          if(ismc) {
-            def mcb = new Bank(factory.getSchema("MC::Particle"))
-            event.read(mcb)
+						def cuts = [ ww>2, q2>2,
+							dpx<0.4, dpy<0.4, dpz>-0.75, dpz<1,
+							dphi<5,
+							//dpx<0.4, dpy<0.4, dpz>-0.5, dpz<0.75,
+							//dphi<4,
+							]
 
-            def gele = LorentzVector.withPID(11,*['px','py','pz'].collect{mcb.getFloat(it,0)})
-            def gqq = beam - gele
-            def gpro = LorentzVector.withPID(2212,*['px','py','pz'].collect{mcb.getFloat(it,1)})
-            def lnorm = gqq.vect().cross(beam.vect())
-            def hnorm = gpro.vect().cross(gqq.vect())
-            def gphi = lnorm.dot(gpro.vect()) > 0 ? 360 - lnorm.theta(hnorm) : lnorm.theta(hnorm)
+						if(cuts.every()) {
+							def status = 0
 
-            px0=gpro.px()
-            py0=gpro.py()
-            pz0=gpro.pz()
+							if(elecan.cut_EC_FIDUCIAL(Level.LOOSE))
+								status += 1
+							if(elecan.cut_EC_FIDUCIAL(Level.TIGHT))
+								status += 2
+							if(procan.cut_CHI2PID())
+								status += 4
+							if(procan.cut_DC_FIDUCIAL_REG1() && procan.cut_DC_FIDUCIAL_REG2() && procan.cut_DC_FIDUCIAL_REG3())
+								status += 8
+							if(g1can.cut_EC_FIDUCIAL(Level.LOOSEST) && g2can.cut_EC_FIDUCIAL(Level.LOOSEST))
+								status += 16
+							if(g1can.cut_EC_FIDUCIAL(Level.LOOSE) && g2can.cut_EC_FIDUCIAL(Level.LOOSE))
+								status += 32
+							if(g1can.cut_BETA() && g2can.cut_BETA())
+								status += 64
 
-            float sign = Math.sin(Math.toRadians(gphi))
-            ihel = Math.random() < (0.5-sign*0.03) ? 1 : -1
-          }
-
-          def cuts = [ ww>2, q2>2,
-                       dpx<0.4, dpy<0.4, dpz>-0.75, dpz<1,
-                       dphi<5,
-                       //dpx<0.4, dpy<0.4, dpz>-0.5, dpz<0.75,
-                       //dphi<4,
-                     ]
+							def vars = [ele.px(),ele.py(),ele.pz(),
+								pro.px(),pro.py(),pro.pz(),
+								g1.px(),g1.py(),g1.pz(),
+								g2.px(),g2.py(),g2.pz(),
+								esec,psec,g1sec,g2sec,
+								run,status
+							]
 
 
-          if(cuts.every()) {
+							if(ismc) {
+								def mcb = new Bank(factory.getSchema("MC::Particle"))
+								event.read(mcb)
 
-/*
-            def stats = [ftheta<1.8, ftheta<2, ftheta<2.2,
-                         dpx<0.18,dpx<0.2,dpx<0.22,
-                         dpy<0.18,dpy<0.2,dpy<0.22]
+								def gele = LorentzVector.withPID(11,*['px','py','pz'].collect{mcb.getFloat(it,0)})
+								def gpro = LorentzVector.withPID(2212,*['px','py','pz'].collect{mcb.getFloat(it,1)})
 
-            def status = stats.withIndex().sum{sys,ind->sys ? 2**ind : 0}
-*/
+								vars += [gele.px(), gele.py(), gele.pz(), gpro.px(), gpro.py(), gpro.pz()]
+							} else {
+								def ihel = evb.getByte("helicity",0)
+								def dcx1=procan.getDC1x(), dcy1=procan.getDC1y(), dcz1=procan.getDC1z()
+								vars += [ihel, dcx1,dcy1,dcz1, g1can.pcal_lv,g1can.pcal_lw, g2can.pcal_lv,g2can.pcal_lw]
+							}
 
-            def status = 0
+							return vars
+						}
 
-            if(canele.cut_EC_FIDUCIAL(Level.LOOSE))
-              status += 1
-            if(canele.cut_EC_FIDUCIAL(Level.TIGHT))
-              status += 2
-            if(canpro.cut_CHI2PID())
-              status += 4
-            if(canpro.cut_DC_FIDUCIAL_REG1() && canpro.cut_DC_FIDUCIAL_REG2() && canpro.cut_DC_FIDUCIAL_REG3())
-              status += 8
-            if(cang1.cut_EC_FIDUCIAL(Level.LOOSEST) && cang2.cut_EC_FIDUCIAL(Level.LOOSEST))
-              status += 16
-            if(cang1.cut_EC_FIDUCIAL(Level.LOOSE) && cang2.cut_EC_FIDUCIAL(Level.LOOSE))
-              status += 32
-            if(cang1.cut_BETA() && cang2.cut_BETA())
-              status += 64
+						return null
+					}
 
+					if(finals) {
+						// def npros = (finals.collect{it[1]} as Set).size()
+						// def npi0s = (finals.collect{it[2..3]} as Set).size()
 
-            //if(dpx<0.2 && dpy<0.2)
-            //if(ftheta<2 && dpx<0.2 && dpy<0.2)
-              return [iele,ipro,ig1,ig2,
-                    ihel,
-                    ele.px(),ele.py(),ele.pz(),
-                    pro.px(),pro.py(),pro.pz(),
-                    g1.px(),g1.py(),g1.pz(),
-                    g2.px(),g2.py(),g2.pz(),
-                    idet,esec,psec,g1sec,g2sec,
-                    run,status,
-                    px0,py0,pz0,
-                    dcx1,dcy1,dcz1,
-                    cang1.pcal_lv,cang1.pcal_lw,
-                    cang2.pcal_lv,cang2.pcal_lw
-                  ]
-          }
+						def fill = finals.max{pp->pp[6..11].sum{it*it}}
+						tt.fill(*fill)
+					}
+				}
+			}
+		}
+	}
 
-          return null
-        }
-
-        if(finals) {
-          def npros = (finals.collect{it[1]} as Set).size()
-          def npi0s = (finals.collect{it[2..3]} as Set).size()
-
-          def fill = finals.max{pp->pp[11..16].sum{it*it}}
-          //println(finals[0][4..-1])
-          //tt.fill(*finals[0][4..-1], npros,npi0s)
-          tt.fill(*fill[4..-1])
-        }
-      }
-    }
-  }
-
-  reader.close()
+	reader.close()
 }
 }
 
