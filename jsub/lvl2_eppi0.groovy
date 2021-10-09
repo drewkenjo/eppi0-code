00034dd1fab3ad7e41e683c324ff59ac837bb8d9
Andrey <kenjo@jlab.org>
2021-10-09 08:34:17 -0400

    update skimming

--- jsub/lvl2_eppi0.groovy
+++ jsub/lvl2_eppi0.groovy
@@ -5,8 +5,6 @@ import org.jlab.jnp.hipo4.data.Bank
 import org.jlab.jnp.hipo4.data.Event
 import org.jlab.jnp.hipo4.data.SchemaFactory
 import org.jlab.clas.physics.LorentzVector
-import uconn.utils.pid.stefan.ElectronCandidate
-
 
 import my.Sugar
 
@@ -38,24 +36,25 @@ while(reader.hasNext()) {
     if(pids[11] && pids[2212] && pids[22])
     if(pids[22].size()>1) {
       def ele = LorentzVector.withPID(11,*['px','py','pz'].collect{partb.getFloat(it,0)})
-      def q2 = (beam-ele).mass2();
-
-      def isepx = pids[2212].collect{it[1]}.any{ipro->
-        def pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{partb.getFloat(it,ipro)})
-        def epx = beam+targ-ele-pro
-        return epx.mass2()<1
-      }
+      def q2 = -(beam-ele).mass2();
 
       def igs = pids[22].collect{it[1]}
-      def ispi0 = [igs,igs].combinations().findAll{ig1,ig2 -> ig2>ig1}
-        .any{ig1,ig2->
-          def g1 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig1)})
-          def g2 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig2)})
-          def pi0 = g1+g2;
+      def ispi0 = pids[2212].collect{it[1]}.any{ipro->
+        def pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{partb.getFloat(it,ipro)})
+        def epx = beam+targ-ele-pro
+        return epx.mass2()<1 && [igs,igs].combinations().findAll{ig1,ig2 -> ig2>ig1}
+          .any{ig1,ig2->
+            def g1 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig1)})
+            def g2 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig2)})
+            def gg = g1+g2;
 
-          return pi0.mass()>0.07 && pi0.mass()<0.2 && q2<-1
-        }
-      if(ispi0 && isepx) writer.addEvent(event)
+            def epggx = beam+targ-ele-pro-gg
+
+            return gg.mass()>0.07 && gg.mass()<0.2 &&
+              epggx.px().abs()<0.5 && epggx.py().abs()<0.5
+          }
+      }
+      if(ispi0 && q2>1) writer.addEvent(event)
     }
   }
 }
--- jsub/lvl3_eppi0.groovy
+++ jsub/lvl3_eppi0.groovy
@@ -5,8 +5,6 @@ import org.jlab.jnp.hipo4.data.Bank
 import org.jlab.jnp.hipo4.data.Event
 import org.jlab.jnp.hipo4.data.SchemaFactory
 import org.jlab.clas.physics.LorentzVector
-import uconn.utils.pid.stefan.ElectronCandidate
-
 
 import my.Sugar
 
@@ -24,7 +22,8 @@ def banknames = ['RUN::config', 'REC::Particle','REC::Calorimeter','REC::Cherenk
 def banks = banknames.collect{new Bank(factory.getSchema(it))}
 
 def writer = new HipoWriter(factory)
-writer.open('lvl1_eppi0.'+fname.split("/")[-1])
+fname = fname.replace('lvl2_eppi0.','')
+writer.open('lvl3_eppi0.'+fname.split("/")[-1])
 
 while(reader.hasNext()) {
   reader.nextEvent(event)
@@ -39,18 +38,25 @@ while(reader.hasNext()) {
     if(pids[22].size()>1) {
       def ele = LorentzVector.withPID(11,*['px','py','pz'].collect{partb.getFloat(it,0)})
       def q2 = -(beam-ele).mass2();
-      def ww = (beam+targ-ele).mass();
+      def ww = (beam-ele+targ).mass();
 
       def igs = pids[22].collect{it[1]}
-      def ispi0 = q2>2 && ww>2 && [igs,igs].combinations().findAll{ig1,ig2 -> ig2>ig1}
-        .any{ig1,ig2->
-          def g1 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig1)})
-          def g2 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig2)})
-          def pi0 = g1+g2;
+      def ispi0 = pids[2212].collect{it[1]}.any{ipro->
+        def pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{partb.getFloat(it,ipro)})
+        def epx = beam+targ-ele-pro
+        return epx.mass2()<1 && [igs,igs].combinations().findAll{ig1,ig2 -> ig2>ig1}
+          .any{ig1,ig2->
+            def g1 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig1)})
+            def g2 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig2)})
+            def gg = g1+g2;
 
-          return pi0.mass()>0.07 && pi0.mass()<0.2
-        }
-      if(ispi0) writer.addEvent(event)
+            def epggx = beam+targ-ele-pro-gg
+
+            return gg.mass()>0.07 && gg.mass()<0.2 &&
+              epggx.px().abs()<0.5 && epggx.py().abs()<0.5
+          }
+      }
+      if(ispi0 && q2>2 && ww>2) writer.addEvent(event)
     }
   }
 }
